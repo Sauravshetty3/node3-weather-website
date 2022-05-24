4d78a508f8c7d06ca50c2ae9aa0c099d67ea643d
Saurav Shetty <akashonline22@gmail.com>
2022-05-24 11:55:15 +0530

    Init Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+    const url =
+        "https://api.openweathermap.org/data/2.5/weather?lat=" +
+        latitude +
+        "&lon=" +
+        longitude +
+        "&appid=8eeb4873f5f38b37ac7656750edd7e78";
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback("Unable to connect to weather service", undefined);
+        } else if (body.error) {
+            callback("Unable to find location", undefined);
+        } else {
+            callback(undefined, "Its currently" + body.main.temp + " degree.");
+        }
+    });
+};
+
+module.exports = forecast;
